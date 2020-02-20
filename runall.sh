162ba7bc3516acd490048848d3e53347844cd29b
Sasha Matijasic <sasha@selectnull.com>
2020-02-20 19:57:19 +0100

    First submission

---
+++ runall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+./books.py data/a_example.txt > output/a_example.out
+./books.py data/b_read_on.txt > output/b_read_on.out
+./books.py data/c_incunabula.txt > output/c_incunabula.out
+./books.py data/d_tough_choices.txt > output/d_tough_choices.out
+./books.py data/e_so_many_books.txt > output/e_so_many_books.out
+./books.py data/f_libraries_of_the_world.txt > output/f_libraries_of_the_world.out
